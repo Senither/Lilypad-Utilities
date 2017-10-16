2684abdf8a045d0fecaa3bfc8be200f72af67898
Alexis Tan <alexis@sen-dev.com>
2017-10-16 19:07:40 +0200

    Add total player count to global list command

--- src/com/senither/lilypadutilities/commands/GlobalListCommad.java
+++ src/com/senither/lilypadutilities/commands/GlobalListCommad.java
@@ -29,15 +29,18 @@ public class GlobalListCommad implements CommandExecutor {
 
         List<String> servers = new ArrayList<>();
 
+        long totalPlayers = 0;
         for (Map.Entry<String, NetworkServer> item : plugin.getNetwork().getNetworkServers().entrySet()) {
             if (item.getValue().isOffline()) {
                 servers.add("&c" + item.getValue().getName());
                 continue;
             }
+            totalPlayers += item.getValue().getPlayers();
             servers.add(String.format("&a%s &7(&f%s&7)", item.getValue().getName(), item.getValue().getPlayers()));
         }
 
         plugin.getEnvoyer().sendMessage(sender, "&6&l[&e+&6&l] &6Global Server List");
+        plugin.getEnvoyer().sendMessage(sender, String.format("&eThere are a total of &6%s &eplayers online.", totalPlayers));
         plugin.getEnvoyer().sendMessage(sender, String.join(
                 "&e, ", servers.stream().sorted().collect(Collectors.toList())
         ));
